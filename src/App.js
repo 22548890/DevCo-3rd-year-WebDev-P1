5b4747c9c2a917becf62d2406d397bc4959c1d84
Sass, MF, Mr [22548890@sun.ac.za] <22548890@sun.ac.za>
2022-03-10 09:56:25 +0200

    company registration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import DevReg from './Components/Dev_Registration';
+import DevReg from './Components/Dev_Registration'
+import ComReg from './Components/Com_Registration'
 import Login from './Components/Login'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
@@ -13,8 +14,11 @@ function App() {
         <Route exact path={"/login"}>
           <Login/>
         </Route>
-        <Route  exact path={"/Dsignup"} component={DevReg}>
+        <Route  exact path={"/Dsignup"} >
           <DevReg/>
+        </Route>
+        <Route  exact path={"/Csignup"} >
+          <ComReg/>
         </Route>
       </Switch>
       {/* <Login/> */}
