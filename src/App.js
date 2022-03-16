86c60cb2b246cedb2cb16097adc62f7dce8e0d42
21586233 <88935451+21586233@users.noreply.github.com>
2022-03-16 20:17:32 +0200

    react verion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CardProfile from './Components/Form';
+
+function App() {
+
+  return (
+    <>
+    <CardProfile/>
+    </>
+  );
+}
+export default App;
---
+++ src/Jamie/Mysql.session.sql
@@ -0,0 +1,16 @@
+INSERT INTO dev (
+    firstname,
+    locations,
+    contractlength,
+    descript,
+    lang,
+    contractval
+  )
+VALUES (
+    'David Sher',
+    'In person',
+    '12 month',
+    'Lazy',
+    'Delphi',
+     10000
+  );SHOW DATABASES;
