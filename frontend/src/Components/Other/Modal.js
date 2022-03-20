60acbcd594848ce4519d6ae2c198bc555b9a1bc4
Genders, R, Mr [23904097@sun.ac.za] <23904097@sun.ac.za>
2022-03-20 22:05:43 +0200

    editing modal

--- frontend/src/Components/Other/Modal.js
+++ frontend/src/Components/Other/Modal.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 import { FaRegUser } from 'react-icons/fa'
+import Developer from './Developer'
 
-const Modal = (contract) => {
-  
+const Modal = ({text}) => {
   return (
     <Popup
-      trigger={<button className="button"> <FaRegUser style={{ color: 'green', cursor: 'pointer' }} /> </button>}
+    trigger={<div className="modal-item"> {text} </div>}
       modal
       nested
     >
@@ -15,30 +15,12 @@ const Modal = (contract) => {
           <button className="close" onClick={close}>
             &times;
           </button>
-          <div className="header"> Developers interested in this contract </div>
-          <div className={`contract `}>
+          <div className="header">Profile</div>
+          <div className={'content'}>
             {' '}
-
+            
             <br />
 
-          </div>
-          <div className="actions">
-            <Popup
-              
-            >
-              <span>
-
-              </span>
-            </Popup>
-            <button
-              className="button"
-              onClick={() => {
-                console.log('modal closed ');
-                close();
-              }}
-            >
-              close modal
-            </button>
           </div>
         </div>
       )}
